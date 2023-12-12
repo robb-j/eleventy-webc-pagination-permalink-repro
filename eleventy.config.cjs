ca7f35b5edb62c773d03e8fe57ea4cc937035688
Rob Anderson <robanderson@hey.com>
2023-12-12 14:47:43 +0000

    try updated syntax

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,5 +1,11 @@
+const { createHmac } = require('node:crypto')
 const eleventyWebc = require('@11ty/eleventy-plugin-webc')
+
+function hash(value) {
+  return createHmac('sha256', 'top_secret').update(value).digest('base64url')
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyWebc)
+  eleventyConfig.addFilter('hash', v => hash(v))
 }
